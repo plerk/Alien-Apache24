624d17a13c4f47460ff0cd99658edfb48fde1c41
Graham Ollis <perl@wdlabs.com>
2015-12-10 21:29:04 -0500

    try the slower method

--- corpus/travis_helper.sh
+++ corpus/travis_helper.sh
@@ -8,12 +8,12 @@ mkdir -p /tmp/build/apr /tmp/build/apr-util
 cd /tmp/build/apr
 curl http://www.motorlogy.com/apache/apr/apr-1.5.2.tar.gz | tar zxvf -
 cd apr-1.5.2
-./configure --prefix=$HOME/tools && make && make install
+./configure && make && sudo make install
 
 which apr-1-config
 
 cd /tmp/build/apr-util
 curl http://www.motorlogy.com/apache/apr/apr-util-1.5.4.tar.gz | tar zxvf -
 cd apr-util-1.5.4
-./configure --prefix=$HOME/tools && make && make install
+./configure && make && sudo make install
 
