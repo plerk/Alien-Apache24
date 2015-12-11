84541d88d49c8c70ee6b709e513f3b3ef574a7a7
Graham Ollis <plicease@cpan.org>
2015-12-10 19:52:25 -0500

    and decompress

--- corpus/travis_helper.sh
+++ corpus/travis_helper.sh
@@ -6,12 +6,12 @@ IFS=$'\n\t'
 mkdir -p /tmp/build/apr /tmp/build/apr-util
 
 cd /tmp/build/apr
-curl http://www.motorlogy.com/apache/apr/apr-1.5.2.tar.gz | tar xvf -
+curl http://www.motorlogy.com/apache/apr/apr-1.5.2.tar.gz | tar zxvf -
 cd apr-1.5.2
 ./configure && make && make install
 
 cd /tmp/build/apr-util
-curl http://www.motorlogy.com/apache/apr/apr-util-1.5.4.tar.gz | tar xvf -
+curl http://www.motorlogy.com/apache/apr/apr-util-1.5.4.tar.gz | tar zxvf -
 cd apr-util-1.5.4
 ./configure && make && make install
 
