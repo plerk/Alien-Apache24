bfbb575e80df4d41b44bb747cdaadc455d656002
Graham Ollis <perl@wdlabs.com>
2015-12-10 21:33:00 -0500

    into usr

--- corpus/travis_helper.sh
+++ corpus/travis_helper.sh
@@ -8,12 +8,10 @@ mkdir -p /tmp/build/apr /tmp/build/apr-util
 cd /tmp/build/apr
 curl http://www.motorlogy.com/apache/apr/apr-1.5.2.tar.gz | tar zxvf -
 cd apr-1.5.2
-./configure && make && sudo make install
-
-which apr-1-config
+./configure --prefix=/usr && make && sudo make install
 
 cd /tmp/build/apr-util
 curl http://www.motorlogy.com/apache/apr/apr-util-1.5.4.tar.gz | tar zxvf -
 cd apr-util-1.5.4
-./configure && make && sudo make install
+./configure --prefix=/usr && make && sudo make install
 
